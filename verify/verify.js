c2056c055a678641f0a8586119cb554592f8bd59
Tihonovec-Elizaveta <el.tihonovec@gmail.com>
2024-06-24 12:46:55 +0300

    added

--- verify/verify.js
+++ verify/verify.js
@@ -29,6 +29,7 @@
             alert('Пароли не совпадают!');
             return;
           }
+          loaderOverlay.classList.remove('hidden'); 
 
       fetch('https://x-bank.alsiberij.com/ms-users/v1/auth/sign-up', {
         method: 'POST',
@@ -39,19 +40,50 @@
       })
       .then(data => {
         if (data.success) { 
-          alert('Аккаунт успешно создан!');
-          window.location.href = 'https://x-bank.alsiberij.com/ms-users/v1/auth/sign-in';
+          alert('Аккаунт успешно создан, проверьте email!');
+          loaderOverlay.classList.add('hidden');
+          // window.location.href = 'https://x-bank.alsiberij.com/ms-users/v1/auth/sign-in';
         } else {
-          alert(data.message || 'Ошибка регистрации'); 
+          alert(data.message || 'Ошибка регистрации!'); 
         }
       })
       .catch(error => {
         console.error('Ошибка:', error);
-        alert('Произошла ошибка при регистрации');
+        alert('Произошла ошибка при регистрации!');
+        loaderOverlay.classList.add('hidden');
       });
     });
-    
  }
 
 
+window.onload = function () {
+async function sendVerificationRequest() {
+  const url = new URL('https://x-bank.alsiberij.com/ms-users/v1/auth/verification?code=123456');
+  const verificationCode = url.searchParams.get('code');
+  if (verificationCode) {
+    try {
+      const response = await fetch('https://x-bank.alsiberij.com/ms-users/v1/auth/verification?code=123456', { 
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ verification: verificationCode })
+      });
 
+      if (response.ok) {
+        console.log('Код верификации успешно отправлен');
+        alert('Аккаунт успешно активирован!');
+        window.location.href = 'https://x-bank.alsiberij.com/ms-users/v1/auth/sign-in';
+      } else {
+        console.error('Ошибка при отправке кода верификации');
+        alert('Ошибка активации!');
+      }
+    } catch (error) {
+      console.error('Ошибка при отправке запроса', error);
+    }
+  } else {
+    console.error('Код верификации не найден в URL');
+  }
+}
+sendVerificationRequest()
+}
