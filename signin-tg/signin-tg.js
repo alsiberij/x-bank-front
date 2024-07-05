bbb2924b7d08f4b2130be41786dcf989399192b3
sibirtsev_a <sibirtsev_a@lwo.by>
2024-07-05 11:24:50 +0300

    fix Bug fixed

--- signin-tg/signin-tg.js
+++ signin-tg/signin-tg.js
@@ -1,28 +1,29 @@
 window.onload = function (){
-const twoFactorAuthenticationToken = localStorage.getItem('twoFactorAuthenticationToken');
-const twoFactorAuthenticationCode = document.getElementById('2FA').value;
+  document.getElementById('signinTgButton').addEventListener('click', async function() {
+    const twoFactorAuthenticationToken = localStorage.getItem('2FA');
+    const twoFactorAuthenticationCode = document.getElementById('twoFactorAuthenticationCode').value;
 
-
-fetch('https://x-bank.alsiberij.com/ms-users/v1/auth/sign-in/2fa', {
-  method: 'POST',
-  headers: {
-    'Authorization': "Bearer " + localStorage.getItem("2FA"),
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ code: '327305' }),
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Ошибка авторизации.');
-    }
-    return response.json();
+    fetch('https://x-bank.alsiberij.com/ms-users/v1/auth/sign-in/2fa', {
+      method: 'POST',
+      headers: {
+        'Authorization': "Bearer " + localStorage.getItem("2FA"),
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ code: twoFactorAuthenticationCode }),
+    })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Ошибка авторизации.');
+          }
+          return response.json();
+        })
+        .then(data => {
+          localStorage.setItem('accessToken', data.tokens.accessToken);
+          localStorage.setItem('refreshToken', data.tokens.refreshToken);
+          window.location.href = '/personalData/personalData.html';
+        })
+        .catch(error => {
+          console.error(error);
+        });
   })
-  .then(data => {
-    localStorage.setItem('accessToken', data.tokens.accessToken);
-    localStorage.setItem('refreshToken', data.tokens.refreshToken);
-    window.location.href = '/personalData/personalData.html';
-  })
-  .catch(error => {
-    console.error(error);
-  });
 }
